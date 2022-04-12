2a9a875a1ac1d27141092fd8287a59a8eb5d54d9
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-04-12 19:06:49 -0400

    first div creation code created for first API

--- assets/script.js
+++ assets/script.js
@@ -2,62 +2,37 @@ var searchInput = document.getElementById('search-city');
 var breweryListEl = document.querySelector(".breweryList");
 
 
-
-
-// var fetchBreweryCity = function(){
-//         let city = searchInput.value.trim();
-//         console.log(city)
-//         let apiURL = ('https://api.openbrewerydb.org/breweries?by_city=' + city)
-//         fetch(apiURL).then(function (response) {
-//             if (response.ok) {
-//                 response.json().then(function (data) {
-//                     console.log(data)
-//                 })
-//             }
-//         })}
+//Brewery List Function/Div Creation
 
 var fetchBreweryCity = function(){
     let city = searchInput.value.trim();
     
-    let apiURL = ('https://api.openbrewerydb.org/breweries?by_city=' + city)
+    let apiURL = ('https://api.openbrewerydb.org/breweries?per_page=15&by_city=' + city)
     fetch(apiURL).then(function (response) {
+        //add some type of clear list here
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
-
-                // var breweryArray = data;
-                // console.log(breweryArray.length)
+                
+                //randomizer needed
                 for (let i = 0; i < data.length; i++) {
-                console.log(data.length)
                 var breweryName = data[i].name;
                 var breweryType = data[i].brewery_type;
-                var website_url = data[i].website_url;
                 var address = data[i].street;
                 var phone = data[i].phone;
-                var state = data[i].state;
-                var postalCode= data[i].postal_code;
-                console.log(data)
-
-                var breweryNameContainer = document.createElement("tr");
-                // breweryNameContainer.innerHTML = breweryName;
-               breweryListEl.appendChild(breweryNameContainer);
-
-               var breweryTypeEl = document.createElement("tr");
-               
            
-            //    breweryTypeEl.classList.add = "class1";
-               breweryTypeEl.innerHTML = ['<td class="class"></td>'];
-               breweryListEl.appendChild(breweryTypeEl);
-                
-              var nameList = document.querySelector(".class1");
-              nameList.innerHTML = breweryName;
-            //   breweryListEl.appendChild(breweryName);
+
+               var breweryNameContainerEl = document.createElement("tr");
+               breweryNameContainerEl.innerHTML = 
+               '<tr> <td class="breweryName"> ' + breweryName +' </td> <td class="address"> ' + address +' </td> <td class="phoneNumber"> ' + phone +' </td> </td> <td class="type"> ' + breweryType +' </tr>';
+                breweryListEl.appendChild(breweryNameContainerEl);
+       
             }
             })
         }
     })}
 
-
+//Food List Function/Div Creation
 // var fetchFoodCity = function(){
 //         let city = searchInput.value.trim();
 //         console.log(city)
@@ -74,17 +49,17 @@ var fetchBreweryCity = function(){
 
 
 //Listener
-        $('.btn').on('click', function(){
-            var searchInput = document.getElementById('search-city').value;
-            if (searchInput !== ''){
-            localStorage.setItem(JSON.stringify(searchInput), JSON.stringify(searchInput));
-            fetchBreweryCity()
-            // fetchFoodCity()
-            }
-            else {
-                window.alert("Please Input a City");
-            }
-            
-            
-            
-        })
+    $('.btn').on('click', function(){
+        var searchInput = document.getElementById('search-city').value;
+        if (searchInput !== ''){
+        localStorage.setItem(JSON.stringify(searchInput), JSON.stringify(searchInput));
+        fetchBreweryCity()
+        // fetchFoodCity()
+        }
+        else {
+            window.alert("Please Input a City");
+        }
+        
+        
+        
+    })
