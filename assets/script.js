b38bf3a934a873b7ca82dead38461150966df35e
chlucas324 <chlucas324@gmail.com>
2022-04-11 20:37:50 -0400

    create inner HTML for api data

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var searchInput = document.getElementById('search-city');
-
+var breweryListEl = document.querySelector(".breweryList");
 
 
 
@@ -24,14 +24,35 @@ var fetchBreweryCity = function(){
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
-                var breweryName = data[0].name;
-                var breweryType = data[0].brewery_type;
-                var website_url = data[0].website_url;
-                var address = data[0].street;
-                var phone = data[0].phone;
-                var state = data[0].state;
-                var postalCode= data[0].postal_code;
-                console.log(breweryName)
+
+                // var breweryArray = data;
+                // console.log(breweryArray.length)
+                for (let i = 0; i < data.length; i++) {
+                console.log(data.length)
+                var breweryName = data[i].name;
+                var breweryType = data[i].brewery_type;
+                var website_url = data[i].website_url;
+                var address = data[i].street;
+                var phone = data[i].phone;
+                var state = data[i].state;
+                var postalCode= data[i].postal_code;
+                console.log(data)
+
+                var breweryNameContainer = document.createElement("tr");
+                // breweryNameContainer.innerHTML = breweryName;
+               breweryListEl.appendChild(breweryNameContainer);
+
+               var breweryTypeEl = document.createElement("tr");
+               
+           
+            //    breweryTypeEl.classList.add = "class1";
+               breweryTypeEl.innerHTML = ['<td class="class"></td>'];
+               breweryListEl.appendChild(breweryTypeEl);
+                
+              var nameList = document.querySelector(".class1");
+              nameList.innerHTML = breweryName;
+            //   breweryListEl.appendChild(breweryName);
+            }
             })
         }
     })}
